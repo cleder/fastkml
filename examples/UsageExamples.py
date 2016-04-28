6785bf283234a35957d1645a3eb79279f9ac0bae
heltonbiker <heltonbiker@gmail.com>
2016-04-28 01:23:38 -0300

    Added function documentation

--- examples/UsageExamples.py
+++ examples/UsageExamples.py
@@ -3,7 +3,8 @@
 
 from fastkml import kml
 
-def print_child_features(element):    
+def print_child_features(element):
+    """ Prints the name of every child node of the given element, recursively """
     if not getattr(element, 'features', None):
         return
     for feature in element.features():
