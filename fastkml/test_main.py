3549899a158950c99dace25b78185ff1827e31cb
MrNaif2018 <chuff184@gmail.com>
2021-10-13 23:53:31 +0300

    Fix formatting

--- fastkml/test_main.py
+++ fastkml/test_main.py
@@ -311,7 +311,9 @@ class BuildKmlTestCase(unittest.TestCase):
         d = kml.Document()
         d.author = "Christian Ledermann"
         self.assertIn("Christian Ledermann", str(d.to_string()))
-        a = atom.Author(name="Nobody", uri="http://localhost", email="cl@donotreply.com")
+        a = atom.Author(
+            name="Nobody", uri="http://localhost", email="cl@donotreply.com"
+        )
         d.author = a
         self.assertEqual(d.author, "Nobody")
         self.assertNotIn("Christian Ledermann", str(d.to_string()))
@@ -817,7 +819,9 @@ class KmlFromStringTestCase(unittest.TestCase):
         sd = kml.SchemaData(ns="", schema_url="#default")
         sd.from_string(doc)
         self.assertEqual(sd.schema_url, "#TrailHeadTypeId")
-        self.assertEqual(sd.data[0], {"name": "TrailHeadName", "value": "Pi in the sky"})
+        self.assertEqual(
+            sd.data[0], {"name": "TrailHeadName", "value": "Pi in the sky"}
+        )
         self.assertEqual(sd.data[1], {"name": "TrailLength", "value": "3.14159"})
         self.assertEqual(sd.data[2], {"name": "ElevationGain", "value": "10"})
         sd1 = kml.SchemaData(ns="", schema_url="#default")
