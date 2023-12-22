cleder/fastkml

commit 84103d97bf18bb9b426a50ef380d9b3ae4d204a7

fix visibility and tests

fastkml/kml.py
@@ -405,8 +405,9 @@ class _Feature(TimeMixin, _BaseObject):
             element.append(self._camera.etree_element())
         elif self.look_at is not None:
             element.append(self._look_at.etree_element())
-        visibility = config.etree.SubElement(element, f"{self.ns}visibility")
-        visibility.text = str(self.visibility)
+        if self.visibility is not None:
+            visibility = config.etree.SubElement(element, f"{self.ns}visibility")
+            visibility.text = str(self.visibility)
         if self.isopen:
             isopen = config.etree.SubElement(element, f"{self.ns}open")
             isopen.text = str(self.isopen)
@@ -453,7 +454,7 @@ class _Feature(TimeMixin, _BaseObject):
         if description is not None:
             self.description = description.text
         visibility = element.find(f"{self.ns}visibility")
-        if visibility is not None:
+        if visibility is not None and visibility.text:
             self.visibility = 1 if visibility.text in ["1", "true"] else 0
         isopen = element.find(f"{self.ns}open")
         if isopen is not None:
@@ -1367,7 +1368,7 @@ class Document(_Container):
                 element.append(schema.etree_element())
         return element
 
-    def get_style_by_url(self, style_url: str) -> Union[Style, StyleMap]:
+    def get_style_by_url(self, style_url: str) -> Optional[Union[Style, StyleMap]]:
         id = urlparse.urlparse(style_url).fragment
         for style in self.styles():
             if style.id == id:
@@ -1529,7 +1530,7 @@ class KML:
         self,
         ns: Optional[str] = None,
         name_spaces: Optional[Dict[str, str]] = None,
-        features: Iterable[Union[Folder, Document, Placemark]] = None,
+        features: Optional[Iterable[Union[Folder, Document, Placemark]]] = None,
     ) -> None:
         """
         The namespace (ns) may be empty ('') if the 'kml:' prefix is
@@ -1596,7 +1597,7 @@ class KML:
             try:
                 root = config.etree.Element(
                     f"{self.ns}kml",
-                    nsmap={None: self.ns[1:-1]},
+                    # nsmap={None: self.ns[1:-1]},
                 )
             except TypeError:
                 root = config.etree.Element(f"{self.ns}kml")

.github/workflows/run-all-tests.yml
@@ -128,13 +128,13 @@ jobs:
           --outdir dist/
       - name: Publish distribution 📦 to Test PyPI for tags
         if: startsWith(github.ref, 'refs/tags')
-        uses: pypa/gh-action-pypi-publish@master
+        uses: pypa/gh-action-pypi-publish@release/v1
         with:
           password: ${{ secrets.TEST_PYPI_API_TOKEN }}
           repository_url: https://test.pypi.org/legacy/
       - name: Publish distribution 📦 to PyPI for push to main
         if: github.event_name == 'push' && github.ref == 'refs/heads/main'
-        uses: pypa/gh-action-pypi-publish@master
+        uses: pypa/gh-action-pypi-publish@release/v1
         with:
           password: ${{ secrets.PYPI_API_TOKEN }}
 ...

fastkml/about.py
@@ -3,4 +3,4 @@ About fastkml.
 
 The only purpose of this module is to provide a version number for the package.
 """
-__version__ = "1.0.a7"
+__version__ = "1.0.a8"

fastkml/links.py
@@ -81,13 +81,8 @@ class Icon(_BaseObject):
         return self._href
 
     @href.setter
-    def href(self, href) -> None:
-        if isinstance(href, str):
-            self._href = href
-        elif href is None:
-            self._href = None
-        else:
-            raise ValueError
+    def href(self, href: Optional[str]) -> None:
+        self._href = href
 
     @property
     def refresh_mode(self) -> Optional[RefreshMode]:
@@ -142,7 +137,7 @@ class Icon(_BaseObject):
         self._view_refresh_mode = view_refresh_mode
 
     @property
-    def view_refresh_time(self):
+    def view_refresh_time(self) -> Optional[float]:
         """
         After camera movement stops, specifies the number of seconds to
         wait before refreshing the view.
@@ -179,7 +174,7 @@ class Icon(_BaseObject):
             raise ValueError
 
     @property
-    def view_format(self):
+    def view_format(self) -> Optional[str]:
         """
         Specifies the format of the query string that is appended to the
         Link's <href> before the file is fetched.
@@ -210,7 +205,7 @@ class Icon(_BaseObject):
         return self._view_format
 
     @view_format.setter
-    def view_format(self, view_format) -> None:
+    def view_format(self, view_format: Optional[str]) -> None:
         if isinstance(view_format, str):
             self._view_format = view_format
         elif view_format is None:
@@ -219,7 +214,7 @@ class Icon(_BaseObject):
             raise ValueError
 
     @property
-    def http_query(self):
+    def http_query(self) -> Optional[str]:
         """
         Appends information to the query string, based on the parameters specified.
 
@@ -232,7 +227,7 @@ class Icon(_BaseObject):
         return self._http_query
 
     @http_query.setter
-    def http_query(self, http_query) -> None:
+    def http_query(self, http_query: Optional[str]) -> None:
         if isinstance(http_query, str):
             self._http_query = http_query
         elif http_query is None:
@@ -248,40 +243,52 @@ class Icon(_BaseObject):
         element = super().etree_element(precision=precision, verbosity=verbosity)
 
         if self._href:
-            href = config.etree.SubElement(element, f"{self.ns}href")
+            href = config.etree.SubElement(  # type: ignore[attr-defined]
+                element,
+                f"{self.ns}href",
+            )
             href.text = self._href
         if self._refresh_mode:
-            refresh_mode = config.etree.SubElement(element, f"{self.ns}refreshMode")
+            refresh_mode = config.etree.SubElement(  # type: ignore[attr-defined]
+                element,
+                f"{self.ns}refreshMode",
+            )
             refresh_mode.text = self._refresh_mode.value
         if self._refresh_interval:
-            refresh_interval = config.etree.SubElement(
+            refresh_interval = config.etree.SubElement(  # type: ignore[attr-defined]
                 element,
                 f"{self.ns}refreshInterval",
             )
             refresh_interval.text = str(self._refresh_interval)
         if self._view_refresh_mode:
-            view_refresh_mode = config.etree.SubElement(
+            view_refresh_mode = config.etree.SubElement(  # type: ignore[attr-defined]
                 element,
                 f"{self.ns}viewRefreshMode",
             )
             view_refresh_mode.text = self._view_refresh_mode.value
         if self._view_refresh_time:
-            view_refresh_time = config.etree.SubElement(
+            view_refresh_time = config.etree.SubElement(  # type: ignore[attr-defined]
                 element,
                 f"{self.ns}viewRefreshTime",
             )
             view_refresh_time.text = str(self._view_refresh_time)
         if self._view_bound_scale:
-            view_bound_scale = config.etree.SubElement(
+            view_bound_scale = config.etree.SubElement(  # type: ignore[attr-defined]
                 element,
                 f"{self.ns}viewBoundScale",
             )
             view_bound_scale.text = str(self._view_bound_scale)
         if self._view_format:
-            view_format = config.etree.SubElement(element, f"{self.ns}viewFormat")
+            view_format = config.etree.SubElement(  # type: ignore[attr-defined]
+                element,
+                f"{self.ns}viewFormat",
+            )
             view_format.text = self._view_format
         if self._http_query:
-            http_query = config.etree.SubElement(element, f"{self.ns}httpQuery")
+            http_query = config.etree.SubElement(  # type: ignore[attr-defined]
+                element,
+                f"{self.ns}httpQuery",
+            )
             http_query.text = self._http_query
 
         return element

tests/data_test.py
@@ -92,7 +92,10 @@ class TestStdLibrary(StdLibrary):
         k1.from_string(k.to_string())
         assert "Schema" in k1.to_string()
         assert "SimpleField" in k1.to_string()
-        assert k1.to_string() == k.to_string()
+        assert k1.to_string().replace("kml:", "").replace(
+            ":kml",
+            "",
+        ) == k.to_string().replace("kml:", "").replace(":kml", "")
 
     def test_schema_data(self) -> None:
         ns = "{http://www.opengis.net/kml/2.2}"

tests/oldunit_test.py
@@ -79,8 +79,8 @@ class TestBaseClasses:
         f = kml._Feature(name="A Feature")
         pytest.raises(NotImplementedError, f.etree_element)
         assert f.name == "A Feature"
-        assert f.visibility == 1
-        assert f.isopen == 0
+        assert f.visibility is None
+        assert f.isopen is None
         assert f._atom_author is None
         assert f._atom_link is None
         assert f.address is None
@@ -140,11 +140,16 @@ class TestBuildKml:
         """KML file with folders."""
         ns = "{http://www.opengis.net/kml/2.2}"
         k = kml.KML()
-        f = kml.Folder(ns, "id", "name", "description")
-        nf = kml.Folder(ns, "nested-id", "nested-name", "nested-description")
+        f = kml.Folder(ns, id="id", name="name", description="description")
+        nf = kml.Folder(
+            ns,
+            id="nested-id",
+            name="nested-name",
+            description="nested-description",
+        )
         f.append(nf)
         k.append(f)
-        f2 = kml.Folder(ns, "id2", "name2", "description2")
+        f2 = kml.Folder(ns, id="id2", name="name2", description="description2")
         k.append(f2)
         assert len(list(k.features())) == 2
         assert len(list(next(iter(k.features())).features())) == 1
@@ -156,9 +161,9 @@ class TestBuildKml:
     def test_placemark(self) -> None:
         ns = "{http://www.opengis.net/kml/2.2}"
         k = kml.KML(ns=ns)
-        p = kml.Placemark(ns, "id", "name", "description")
+        p = kml.Placemark(ns, id="id", name="name", description="description")
         # XXX p.geometry = Point(0.0, 0.0, 0.0)
-        p2 = kml.Placemark(ns, "id2", "name2", "description2")
+        p2 = kml.Placemark(ns, id="id2", name="name2", description="description2")
         # XXX p2.geometry = LineString([(0, 0, 0), (1, 1, 1)])
         k.append(p)
         k.append(p2)
@@ -170,17 +175,22 @@ class TestBuildKml:
     def test_document(self) -> None:
         k = kml.KML()
         ns = "{http://www.opengis.net/kml/2.2}"
-        d = kml.Document(ns, "docid", "doc name", "doc description")
-        f = kml.Folder(ns, "fid", "f name", "f description")
+        d = kml.Document(ns, id="docid", name="doc name", description="doc description")
+        f = kml.Folder(ns, id="fid", name="f name", description="f description")
         k.append(d)
         d.append(f)
-        nf = kml.Folder(ns, "nested-fid", "nested f name", "nested f description")
+        nf = kml.Folder(
+            ns,
+            id="nested-fid",
+            name="nested f name",
+            description="nested f description",
+        )
         f.append(nf)
-        f2 = kml.Folder(ns, "id2", "name2", "description2")
+        f2 = kml.Folder(ns, id="id2", name="name2", description="description2")
         d.append(f2)
-        p = kml.Placemark(ns, "id", "name", "description")
+        p = kml.Placemark(ns, id="id", name="name", description="description")
         # XXX p.geometry = Polygon([(0, 0, 0), (1, 1, 0), (1, 0, 1)])
-        p2 = kml.Placemark(ns, "id2", "name2", "description2")
+        p2 = kml.Placemark(ns, id="id2", name="name2", description="description2")
         # p2 does not have a geometry!
         f2.append(p)
         nf.append(p2)
@@ -715,7 +725,7 @@ class TestStyleUsage:
 
         expected = """
             <kml:Document xmlns:kml="http://www.opengis.net/kml/2.2">
-              <kml:visibility>1</kml:visibility>
+              <kml:visibility/>
                 <kml:Style>
                   <kml:PolyStyle>
                     <kml:color>7f000000</kml:color>
@@ -743,7 +753,6 @@ class TestStyleUsage:
 
         expected = """
             <kml:Placemark xmlns:kml="http://www.opengis.net/kml/2.2">
-              <kml:visibility>1</kml:visibility>
                 <kml:Style>
                   <kml:PolyStyle>
                     <kml:color>7f000000</kml:color>
@@ -1527,7 +1536,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "</kml:GroundOverlay>",
         )
         assert g.to_string() == expected.to_string()
@@ -1542,7 +1550,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:color>00010203</kml:color>"
             "<kml:drawOrder>1</kml:drawOrder>"
             "<kml:Icon>"
@@ -1560,7 +1567,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:altitude>123</kml:altitude>"
             "<kml:altitudeMode>clampToGround</kml:altitudeMode>"
             "</kml:GroundOverlay>",
@@ -1575,7 +1581,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:altitude>123.4</kml:altitude>"
             "<kml:altitudeMode>clampToGround</kml:altitudeMode>"
             "</kml:GroundOverlay>",
@@ -1590,7 +1595,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:altitude>123.4</kml:altitude>"
             "<kml:altitudeMode>clampToGround</kml:altitudeMode>"
             "</kml:GroundOverlay>",
@@ -1606,7 +1610,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:altitude>123.4</kml:altitude>"
             "<kml:altitudeMode>absolute</kml:altitudeMode>"
             "</kml:GroundOverlay>",
@@ -1622,7 +1625,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:altitude>123.4</kml:altitude>"
             "<kml:altitudeMode>clampToGround</kml:altitudeMode>"
             "</kml:GroundOverlay>",
@@ -1638,7 +1640,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:altitude>123.4</kml:altitude>"
             "<kml:altitudeMode>clampToGround</kml:altitudeMode>"
             "</kml:GroundOverlay>",
@@ -1653,7 +1654,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:LatLonBox>"
             "<kml:north>10</kml:north>"
             "<kml:south>20</kml:south>"
@@ -1673,7 +1673,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:LatLonBox>"
             "<kml:north>10</kml:north>"
             "<kml:south>20</kml:south>"
@@ -1697,7 +1696,6 @@ class TestGroundOverlayString:
         expected = kml.GroundOverlay()
         expected.from_string(
             '<kml:GroundOverlay xmlns:kml="http://www.opengis.net/kml/2.2">'
-            "<kml:visibility>1</kml:visibility>"
             "<kml:LatLonBox>"
             "<kml:north>10</kml:north>"
             "<kml:south>20</kml:south>"

commit 51e21d0e081f2e62e99187dd22538dcb750cafa4

reenable nsmap for root element
Makes it possible (again) to avoid the `kml:` prefix on every single tag
in the XML output.

Fixes: 84103d97bf18 ("fix visibility and tests")
Fixes: 768a44c7a7a8 ("Refactor XML subelement handling in KML class")

fastkml/kml.py
@@ -85,11 +85,12 @@ class KML(_XMLObject):
             root = config.etree.Element(f"{self.ns}kml")  # type: ignore[attr-defined]
             root.set("xmlns", config.KMLNS[1:-1])
         else:
-            try:
+            if hasattr(config.etree, "LXML_VERSION"):
                 root = config.etree.Element(  # type: ignore[attr-defined]
                     f"{self.ns}kml",
+                    nsmap={None: self.ns[1:-1]},
                 )
-            except TypeError:
+            else:
                 root = config.etree.Element(  # type: ignore[attr-defined]
                     f"{self.ns}kml",
                 )