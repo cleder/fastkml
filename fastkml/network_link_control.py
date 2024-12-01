4f4a3f0ac184081702cf7ee72d920d5d33e43fd5
apurvabanka <appu.banka@gmail.com>
2024-12-01 01:24:49 -0500

    test cases

--- fastkml/network_link_control.py
+++ fastkml/network_link_control.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2012-2024 Christian Ledermann
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+import logging
+
 from typing import Any
 from typing import Dict
 from typing import Optional
@@ -14,6 +31,13 @@ from fastkml.registry import RegistryItem
 from fastkml.registry import registry
 from fastkml.times import KmlDateTime
 from fastkml.views import Camera, LookAt
+
+
+__all__ = [
+    "NetworkLinkControl",
+]
+
+logger = logging.getLogger(__name__)
 
 
 class NetworkLinkControl(_XMLObject):
---
+++ tests/network_link_control_test.py
@@ -0,0 +1,85 @@
+# Copyright (C) 2021 - 2022  Christian Ledermann
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+"""Test the Network Link Control classes."""
+
+import datetime
+
+import pytest
+
+from fastkml.kml import KML
+from fastkml.network_link_control import NetworkLinkControl
+from fastkml.times import KmlDateTime
+from tests.base import StdLibrary
+from fastkml import views
+
+class TestStdLibrary(StdLibrary):
+    """Test with the standard library."""
+
+    def test_network_link_control_obj(self) -> None:
+        dt = datetime.datetime.now()
+        kml_datetime = KmlDateTime(dt=dt)
+        view = views.Camera()
+
+        network_control_obj = NetworkLinkControl(
+            min_refresh_period=1.1,
+            max_session_length=100.1,
+            cookie="cookie",
+            message="message",
+            link_name="link_name",
+            link_description="link_description",
+            link_snippet="link_snippet",
+            expires=kml_datetime,
+            view=view
+        )
+
+        assert network_control_obj.min_refresh_period == 1.1
+        assert network_control_obj.max_session_length == 100.1
+        assert network_control_obj.cookie == "cookie"
+        assert network_control_obj.message == "message"
+        assert network_control_obj.link_name == "link_name"
+        assert network_control_obj.link_description == "link_description"
+        assert network_control_obj.link_snippet == "link_snippet"
+        assert network_control_obj.expires == kml_datetime
+
+        assert network_control_obj.view == view
+
+    def test_network_link_control_kml(self) -> None:
+        doc = (
+            '<kml:NetworkLinkControl xmlns:kml="http://www.opengis.net/kml/2.2">'
+            "<kml:minRefreshPeriod>43200</kml:minRefreshPeriod>"
+            "<kml:maxSessionLength>-1</kml:maxSessionLength>"
+            "<kml:linkSnippet>A Snippet</kml:linkSnippet>"
+            "<kml:expires>2008-05-30</kml:expires>"
+            "</kml:NetworkLinkControl>"
+        )
+
+        network_control = NetworkLinkControl.from_string(doc)
+
+        dt = datetime.date(2008, 5, 30)
+        kml_datetime = KmlDateTime(dt=dt)
+
+        network_control_obj = NetworkLinkControl(
+            min_refresh_period=43200,
+            max_session_length=-1,
+            link_snippet="A Snippet",
+            expires=kml_datetime,
+        )
+
+        assert network_control.min_refresh_period == str(network_control_obj.min_refresh_period)
+        assert network_control.max_session_length == str(network_control_obj.max_session_length)
+        assert network_control.link_snippet == network_control_obj.link_snippet
+        assert network_control.expires == network_control_obj.expires
